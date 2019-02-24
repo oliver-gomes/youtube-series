ea72306b3c0660979139356f19d6c59aefe54d84
Oliver Gomes <ogomesphoto@gmail.com>
2019-02-24 01:26:37 -0500

    01 design code

--- 01-landing-page/src/App.js
+++ 01-landing-page/src/App.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <section>
+          <header>
+            <img src="./images/logo.png" width="100" alt="logo" />
+            <div>
+              <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>SERVICES</li>
+                <li>BLOG</li>
+              </ul>
+            </div>
+          </header>
+        </section>
+
+        <section id="main">
+          <div className="main-text">
+            <span>kindself.</span> <br /> where we reinvent <br />
+            the meaning of selfcare
+          </div>
+
+          <img src="./images/leaf.png" width="520" alt="leaf-main-image" />
+        </section>
       </div>
     );
   }
